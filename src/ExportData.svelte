47a48f60fcb98e7e182bf27e472c4ad156d0c90d
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-11-01 16:28:28 +0100

    Make export data more useable

--- src/ExportData.svelte
+++ src/ExportData.svelte
@@ -1,5 +1,10 @@
-<script style="ts">
+<script lang="ts">
   import { config } from "./data-suggestions";
+  var ctrl: HTMLTextAreaElement;
+
+  function selectAll() {
+    ctrl.select();
+  }
 
   let lines = [];
 
@@ -8,4 +13,16 @@
   lines.push("export const config: { suggestions: Suggestion[] } = ");
 </script>
 
-<textarea>{lines.join('\n')}{JSON.stringify(config, undefined, 2)}{';'}</textarea>
+<style>
+  textarea {
+    width: 100%;
+    height: calc(100vh - 80px);
+    font-family: "Consolas", monospace;
+    font-size: 10pt;
+  }
+</style>
+
+<textarea
+  readonly
+  on:click={selectAll}
+  bind:this={ctrl}>{lines.join('\n')}{JSON.stringify(config, undefined, 2)}{';'}</textarea>
