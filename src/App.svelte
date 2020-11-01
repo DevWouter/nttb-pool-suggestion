a3525ae81b14858678255900b0f90e3634008701
Wouter Lindenhof <wouterlindenhof@gmail.com>
2020-11-01 16:31:01 +0100

    Improve header

--- src/App.svelte
+++ src/App.svelte
@@ -9,9 +9,19 @@
   const SUGGESTION_REGEX = /^suggestion\/(.+)$/;
 </script>
 
-<Link href="/">Overview</Link>
-<Link href="/export">Export</Link>
-<hr>
+<style>
+  .seperator {
+    padding-left: 8px;
+    padding-right: 8px;
+  }
+</style>
+
+<div class="header">
+  <Link href="/">Overview</Link>
+  <span class="seperator">|</span>
+  <Link href="/export">Export</Link>
+</div>
+<hr />
 {#if location == ''}
   <ListSuggestions />
 {/if}
